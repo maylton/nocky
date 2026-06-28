a17a21710a4e539e593ce7b2b2c260d85341e6a4
maylton <maylton.fernandes@gmail.com>
2026-06-28 15:39:20 -0300

    use final stream source settings shell

--- src/ui/settings.rs
+++ src/ui/settings.rs
@@ -2,9 +2,9 @@
 
 #[path = "settings/page.rs"]
 mod page;
-#[path = "settings/shell_clean.rs"]
-mod shell_clean;
+#[path = "settings/shell_final.rs"]
+mod shell_final;
 #[path = "settings/stream_sources.rs"]
 mod stream_sources;
 
-pub(crate) use shell_clean::SettingsPage;
+pub(crate) use shell_final::SettingsPage;
