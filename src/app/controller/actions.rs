ec2ad0c1e018da5ad571dab372d95a65dbda8759
maylton <maylton.fernandes@gmail.com>
2026-07-06 18:43:56 -0300

    refactor(connect): avoid duplicate desktop service startup

--- src/app/controller/actions.rs
+++ src/app/controller/actions.rs
@@ -106,7 +106,6 @@ impl AppController {
         app.add_action(&focus_search);
 
         self.install_nocky_connect_action(app);
-        self.start_nocky_connect_services();
 
         let settings = gio::SimpleAction::new("settings", None);
         {
